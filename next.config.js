0455fae59f1e559dd2cd9290065213f4953ade9c
Loi Ly <lyhuuloi@gmail.com>
2024-12-27 15:00:33 +0700

    Initial commit from your app

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['profile.3fs.vn'],
+    unoptimized: true,  // Thêm dòng này
   },
   env: {
     COMPANY_NAME: '3F Solutions Co., Ltd.',
     COMPANY_SHORT_NAME: '3FS',
     COMPANY_WEBSITE: '3fs.vn'
   },
-	output: 'export', 
+	output: 'export',
+	
 }
 
 module.exports = nextConfig
